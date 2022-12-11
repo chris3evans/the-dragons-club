26ab4848da47ad2190af22ba7ab131923166051f
chris3evans <chris3evans.ce@gmail.com>
2022-12-11 11:52:20 +0000

    Added gitignore and README files, removed commented code

--- JavaScript/accordian.js
+++ JavaScript/accordian.js
@@ -2,16 +2,13 @@ const allQuestions = document.querySelectorAll(".accordian__question");
 const allAnswers = document.querySelectorAll(".accordian__answer");
 
 document.addEventListener("click", function (e) {
-  // ID of question to help find correct answer
   const targetQID = e.target.closest(".accordian__question").id;
 
   const answers = Array.from(allAnswers);
 
-  // DOM element of the answer to be revealed
   const answer = answers.find(function (answer) {
     return answer.id === targetQID;
   });
 
-  // Hide or show depending on current status
   answer.classList.toggle("active");
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# The Dragons Club
+
+A fully responsive page I built for a client who wanted a template page for a crypto site that they were planning on launching. A smooth scroll feature was added to the navigation menu at the top of the page as well.
+
+![The Dragons Club Hero View](/tdc-view-1.png?raw=true)
+![The Dragons Club Page View](/tdc-view-2.png?raw=true)
+
+## Getting Started
+
+1.) Run "npm install"
+
+2.) Right click on "index.html" and select "Open with Live Server"
+
+## Tech Stack
+
+- JavaScript
+- HTML5 & CSS3
+- SASS/SCSS
